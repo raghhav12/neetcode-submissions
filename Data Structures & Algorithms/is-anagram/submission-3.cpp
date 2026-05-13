467c8d2750e83a1d80aa14284b2bd69a1770726d
Raghav Sehgal <raghhavsehgal12@gmail.com>
2026-05-13 15:09:38 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        unordered_map<char,int> m1;
+        unordered_map<char,int> m2;
+        int n = s.length();
+
+        for(int i=0;i<n;i++) {
+            m1[s[i]]++;
+            m2[t[i]]++;
+        }
+
+        return m1==m2;
+    }
+};
