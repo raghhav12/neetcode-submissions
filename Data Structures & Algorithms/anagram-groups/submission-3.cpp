c57563293ce98c259a027e4f89871d59ff746514
Raghav Sehgal <raghhavsehgal12@gmail.com>
2026-05-20 00:07:39 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        map<vector<int>, vector<string>> mp;
+
+        for(int i = 0;i<strs.size();i++) {
+            vector<int> arr(26,0);
+
+            for(int j = 0;j<strs[i].size();j++) {
+                arr[strs[i][j]-'a']++;
+            }
+            mp[arr].push_back(strs[i]);
+        }
+
+        for(auto& kv: mp) {
+            ans.push_back(kv.second);
+        }
+        return ans;
+
+    }
+};
