fcc31dbe71602d5d836ea16d0d445c527c49df83
Raghav Sehgal <raghhavsehgal12@gmail.com>
2026-06-12 01:28:11 +0530

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string str) {
+        stack<char> s;
+        for(int i =0;i<str.length();i++) {
+            char ch = str[i];
+            if (ch == '(' || ch == '[' || ch == '{') {
+                s.push(ch);
+            } else {
+                if (s.empty()) return false;
+                if ((ch == '}' && s.top() != '{') || 
+                (ch == ')' && s.top() != '(') || 
+                (ch == ']' && s.top() != '[')) 
+            {
+                return false;
+            } else {
+                s.pop();
+            }
+            }
+        }
+        return s.size() == 0;
+    }
+};
