c214e129bc33acc91a073768f9c563ad287a26b5
Raghav Sehgal <raghhavsehgal12@gmail.com>
2026-06-23 23:20:50 +0530

    Add: car-fleet - submission-16

---
+++ Data Structures & Algorithms/car-fleet/submission-16.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<pair<int,double>> p;
+       for(int i =0 ;i<position.size();i++) {
+            double time = (double) (target - position[i]) /speed[i];
+            p.push_back({position[i],time});
+       }
+       sort(p.begin(),p.end());
+       
+       double currTime = 0;
+       int ans = 0;
+
+       for(int i = p.size() -1 ;i>=0;i--) {
+        double time = p[i].second;
+
+        if(time > currTime) {
+            ans++;
+            currTime = time;
+        }
+    }
+    return ans;
+    }
+};
