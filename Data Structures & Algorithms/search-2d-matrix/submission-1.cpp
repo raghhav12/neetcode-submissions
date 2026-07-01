b5187daafcd8a0ef679c3754537ec63570dd2d59
Raghav Sehgal <raghhavsehgal12@gmail.com>
2026-07-01 23:59:38 +0530

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& arr, int t) {
+        int n = arr.size();
+
+        for(int i = 0;i<n;i++) {
+            int m = arr[i].size();
+            int l = 0, h = m-1;
+            if (t < arr[i][l]) return false;
+            if (t > arr[i][h]) continue;
+            else {
+                while(l <= h) {
+                    int mid = l + ((h-l)/2);
+                    if(arr[i][mid] == t) {
+                        return true;
+                    } else if (arr[i][mid] > t) {
+                        h = mid - 1;
+                    } else {
+                        l = mid + 1;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
